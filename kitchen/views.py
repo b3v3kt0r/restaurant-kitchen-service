8c3db342e8432bff92ad9b0b71d4bf8a2964bcc0
Stanislav Sudakov <stasiksudakov@gmail.com>
2024-07-18 16:54:53 +0300

    add base.html to template and connected it to bootstrap4 & create index view

--- kitchen/views.py
+++ kitchen/views.py
@@ -2,5 +2,16 @@ from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 
+from .models import DishType, Dish, Cook
+
+
 def index(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("<h1>Hello</h1>")
+    num_dishes = Dish.objects.count()
+    num_dish_type = DishType.objects.count()
+    num_cooks = Cook.objects.count()
+    context = {
+        "num_dishes": num_dishes,
+        "num_dish_type": num_dish_type,
+        "num_cooks": num_cooks
+    }
+    return render(request, "base.html", context)
