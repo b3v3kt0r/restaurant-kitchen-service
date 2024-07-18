7d22a41db3ba3efaf9ef48688aa097af3c13542d
Stanislav Sudakov <stasiksudakov@gmail.com>
2024-07-18 14:36:46 +0300

    customization of admin panel & little update in models

--- kitchen/models.py
+++ kitchen/models.py
@@ -31,9 +31,12 @@ class Dish(models.Model):
     def __str__(self):
         return f"{self.name} (price: {self.price}, dish type: {self.dish_type.name})"
 
+
 class Cook(AbstractUser):
-    years_of_experience = models.IntegerField(validators=[MinValueValidator(0)])
+    years_of_experience = models.IntegerField(default=0, validators=[MinValueValidator(0)])
 
     class Meta:
         ordering = ["username"]
 
+    def __str__(self):
+        return f"{self.username}: {self.first_name} {self.last_name}"
