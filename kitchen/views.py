f605dad11584bc8a4ecf3ffcffa38f992e59aea1
Stanislav Sudakov <stasiksudakov@gmail.com>
2024-07-19 18:22:02 +0300

    implemented create/update/delete feature for dish-type & little cod fixes in other templates

--- kitchen/views.py
+++ kitchen/views.py
@@ -35,6 +35,19 @@ class DishTypeCreateView(LoginRequiredMixin, generic.CreateView):
     template_name = "kitchen/dish_type_form.html"
 
 
+class DishTypeUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = DishType
+    fields = "__all__"
+    success_url = reverse_lazy("kitchen:dish_type_list")
+    template_name = "kitchen/dish_type_form.html"
+
+
+class DishTypeDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = DishType
+    success_url = reverse_lazy("kitchen:dish_type_list")
+    template_name = "kitchen/dish_type_confirm_delete.html"
+
+
 class DishListView(LoginRequiredMixin, generic.ListView):
     model = Dish
     template_name = "kitchen/dish_list.html"
