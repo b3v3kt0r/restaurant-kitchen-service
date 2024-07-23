6d7ab70c4c92c1a76609b15cf27c9459a118bc17
Stanislav Sudakov <stasiksudakov@gmail.com>
2024-07-23 17:12:45 +0300

    updated requirements

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -o errexit
 
 pip install -r requirements.txt
 
-# Convert static asset files
+
 python manage.py collectstatic --no-input
 
 
--- restaurant_kitchen_service/settings.py
+++ restaurant_kitchen_service/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "restaurant-kitchen-service-t6uy.onrender.com"]
 
 # Application definition
 
